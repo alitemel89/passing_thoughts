d78743dcceb29fe153410911cc5aff729f11c410
alitemel89 <alitemel89.ce@gmail.com>
2020-11-11 12:39:37 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { generateId, getNewExpirationTime } from '../utils/helpers';
+import { AddThoughtForm } from './AddThoughtForm';
+
+function App() {
+    const [thoughts, setThoughts] = useState([
+        {
+            id: generateId(),
+            text: 'This is your first',
+            expiresAt: getNewExpirationTime()
+
+        },
+        {
+            id: generateId(),
+            text: 'This is your second',
+            expiresAt: getNewExpirationTime()
+
+        }
+    ]);
+
+    const addThought = (thought) => {
+        setThoughts(prev => [thought, ...prev])
+    }
+    
+  
+    return (
+        <div className="App">
+            <header>
+                <h1>Passing Thoughts</h1>
+            </header>
+
+            <main>
+                <AddThoughtForm />
+            </main>
+        </div>
+
+    )    
+      
+}
+
+export default App;
+
+
